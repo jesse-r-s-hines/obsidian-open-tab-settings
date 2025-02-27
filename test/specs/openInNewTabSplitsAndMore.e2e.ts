34556eaa11bc1e26d9efe8dc186da931d961efb6
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-02-26 21:40:58 -0500

    Add window tests

--- test/specs/openInNewTabSplitsAndMore.e2e.ts
+++ test/specs/openInNewTabSplitsAndMore.e2e.ts
@@ -42,4 +42,32 @@ describe('Test open in new tab for splits and more', () => {
         const loopParent = await workspacePage.getLeafParent("Loop.md");
         expect(bParent).to.eql(loopParent);
     })
+
+    it("test new tabs in new windows", async () => {
+        await workspacePage.openFile("A.md");
+        await workspacePage.openFile("D.md");
+        await workspacePage.setActiveFile("D.md")
+        await browser.executeObsidianCommand("workspace:move-to-new-window");
+        await workspacePage.setActiveFile("A.md")
+
+        const mainWindow = await browser.getWindowHandle();
+        const otherWindow = (await browser.getWindowHandles()).find(h => h != mainWindow)!;
+        
+        (await workspacePage.getLink("B")).click();
+        await browser.waitUntil(async () => (await workspacePage.getAllLeaves()).length >= 3)
+        const aRoot = await workspacePage.getLeafRoot("A.md");
+        const bRoot = await workspacePage.getLeafRoot("B.md");
+        expect(aRoot).to.eql(bRoot)
+
+        await browser.switchToWindow(otherWindow);
+        await workspacePage.setActiveFile("D.md");
+        await (await workspacePage.getLink("Loop")).click()
+        await browser.waitUntil(async () => (await workspacePage.getAllLeaves()).length >= 4)
+        const dRoot = await workspacePage.getLeafRoot("D.md");
+        const loopRoot = await workspacePage.getLeafRoot("Loop.md");
+        expect(dRoot).to.eql(loopRoot)
+        expect(aRoot).to.not.eql(dRoot)
+
+        await browser.switchToWindow(mainWindow)
+    })
 })

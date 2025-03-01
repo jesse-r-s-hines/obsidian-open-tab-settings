7fc0b9280734753b838f8e0b8dda153d4309ca07
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-03-01 18:44:59 -0500

    Add deduplicate

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import { App, Plugin, PluginSettingTab, Setting, Workspace } from 'obsidian';
+import {
+    App, Plugin, PluginSettingTab, Setting, Workspace, WorkspaceLeaf, WorkspaceRoot, WorkspaceFloating,
+    EditableFileView,
+} from 'obsidian';
 import * as monkeyAround from 'monkey-around';
 
 // Remember to rename these classes and interfaces!
 
 export interface MyPluginSettings {
-    openInNewTab: boolean;
+    openInNewTab: boolean,
+    deduplicateTabs: boolean,
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
     openInNewTab: true,
+    deduplicateTabs: true,
 }
 
 export default class MyPlugin extends Plugin {
@@ -23,12 +28,12 @@ export default class MyPlugin extends Plugin {
         // Patch getLeaf to always open in new tab
         const plugin = this
         this.monkeyPatches.push(
-            monkeyAround.around(this.app.workspace.constructor.prototype, {
-                getLeaf(oldMethod) {
-                    return function(this: Workspace, newLeaf?: string|boolean, ...args: any[]) {
+            monkeyAround.around(Workspace.prototype, {
+                getLeaf(oldMethod: any) {
+                    return function(this: Workspace, newLeaf, ...args) {
                         // newLeaf false or undefined means open in current tab. Here we replace those with 'tab' to
                         // always open in new tab.
-                        if (!newLeaf && plugin.settings.openInNewTab) {
+                        if (plugin.settings.openInNewTab && !newLeaf) {
                             const leaf = oldMethod.call(this, 'tab', ...args);
                             // Force focusing the new tab even if focusNewTab is false.
                             if (!(plugin.app.vault as any).getConfig('focusNewTab')) {
@@ -36,9 +41,53 @@ export default class MyPlugin extends Plugin {
                                 plugin.app.workspace.setActiveLeaf(leaf, {focus: true});
                             }
                             return leaf
-                        } else { // no change to behavior
-                            return oldMethod.call(this, newLeaf, ...args);
                         }
+
+                        // use default behavior
+                        return oldMethod.call(this, newLeaf, ...args);
+                    }
+                }
+            })
+        );
+
+        this.monkeyPatches.push(
+            monkeyAround.around(WorkspaceLeaf.prototype, {
+                openFile(oldMethod: any) {
+                    return async function(this: WorkspaceLeaf, file, ...args) {
+                        if (plugin.settings.deduplicateTabs) {
+                            // Check if there are any duplicate tabs
+                            const matches: WorkspaceLeaf[] = [];
+                            plugin.app.workspace.iterateAllLeaves(leaf => {
+                                const root = leaf.getRoot();
+                                // Only check files in the main area or floating windows, not sidebars
+                                const isMainLeaf = (root instanceof WorkspaceRoot || root instanceof WorkspaceFloating);
+                                // Check that the file path matches and its a normal file leaf. "FileView" type includes
+                                // views like outgoing-links which we don't want to switch to. EditableFileView includes
+                                // Markdown, PDF, and images but not views like outgoing-links.
+                                const fileMatch = (
+                                    leaf.view instanceof EditableFileView &&
+                                    leaf.view.file?.path == file.path
+                                );
+                                if (isMainLeaf && fileMatch) {
+                                    matches.push(leaf);
+                                }
+                            });
+
+                            if (matches.length > 0) {
+                                // switch to first matching leaf
+                                await plugin.app.workspace.setActiveLeaf(matches[0], {focus: true})
+                                // If openInNewTab is also enabled, then it will be called first and make a new tab.
+                                // Here we just close the tab before we jump to the existing one.
+                                // TODO: Is there a cleaner way to do this?
+                                if (this.view.getViewType() == "empty") {
+                                    this.detach();
+                                }
+                                return oldMethod.call(matches[0], file, ...args);
+                            }
+                        }
+
+                        // use default behavior
+                        return oldMethod.call(this, file, ...args)
                     }
                 }
             })

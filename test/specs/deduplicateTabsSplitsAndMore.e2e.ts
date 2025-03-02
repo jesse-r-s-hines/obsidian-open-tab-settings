0e0935c13212c1eeced94592f9a8d990e748903d
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-03-02 17:31:49 -0500

    More tests

---
+++ test/specs/deduplicateTabsSplitsAndMore.e2e.ts
@@ -0,0 +1,32 @@
+import { browser } from '@wdio/globals'
+import { expect } from 'chai';
+import workspacePage from 'test/pageobjects/workspace.page';
+import { setSettings, sleep } from './helpers';
+
+
+describe('Test deduplicate for splits and more', () => {
+    beforeEach(async () => {
+        await workspacePage.loadWorkspaceLayout("empty");
+        await setSettings({ openInNewTab: false, deduplicateTabs: true });
+        await workspacePage.setConfig('focusNewTab', false);
+    });
+
+    it('open to the right', async () => {
+        await workspacePage.openFile("A.md");
+        await workspacePage.openFile("B.md");
+        const b1 = await workspacePage.getActiveLeafId();
+        await workspacePage.setActiveFile("A.md");
+        await workspacePage.openLinkToRight(await workspacePage.getLink("B"));
+
+        await browser.waitUntil(async () => 
+            (await workspacePage.getAllLeaves()).length == 3 && (await workspacePage.getActiveLeaf())[1] == "B.md"
+        )
+        // Should create a new tab despite deduplicate
+        expect(await workspacePage.getActiveLeaf()).to.eql(["markdown", "B.md"])
+        expect(await workspacePage.getAllLeaves()).to.eql([["markdown", "A.md"], ["markdown", "B.md"], ["markdown", "B.md"]])
+
+        const b2 = await workspacePage.getActiveLeafId();
+        expect(b1).to.not.eql(b2);
+        expect(await workspacePage.getLeafParent(b1)).to.not.eql(await workspacePage.getLeafParent(b2));
+    })
+})

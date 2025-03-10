57a6f8feb573ad4544cc7b9ab0e9fb2e337435ed
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-03-10 19:58:42 -0400

    Use env vars in wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,15 +1,26 @@
 import * as path from "path"
 
+let versions: [string, string][]; // [appVersion, installerVersion][]
+if (process.env['OBSIDIAN_VERSIONS']) {
+    const appVersions = process.env['OBSIDIAN_VERSIONS'].split(/[ ,]+/);
+    const installerVersions = process.env['OBSIDIAN_INSTALLER_VERSIONS']?.split(/[ ,]+/);
+    if (installerVersions && appVersions.length != installerVersions.length) {
+        throw Error("OBSIDIAN_VERSIONS and OBSIDIAN_INSTALLER_VERSIONS must be the same length");
+    }
+    versions = appVersions.map((v, i) => [v, installerVersions?.[i] ?? 'earliest']);
+} else {
+    versions = [["earliest", "earliest"], ["latest", "latest"]]
+}
+
 export const config: WebdriverIO.Config = {
     runner: 'local',
-    tsConfigPath: './test/tsconfig.json',
-    
+
     specs: [
         './test/specs/**/*.e2e.ts'
     ],
 
     // How many instances of Obsidian should be launched in parallel during testing.
-    maxInstances: 4,
+    maxInstances: Number(process.env["WDIO_MAX_INSTANCES"] ?? 2),
 
     capabilities: [{
         browserName: 'obsidian',

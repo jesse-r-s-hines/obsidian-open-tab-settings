8e5fa64dacd31c217e20d0b5b5bf2d52ba80f7b9
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-05-22 19:20:55 -0400

    Add tests for focusNewTab setting

--- test/specs/helpers.ts
+++ test/specs/helpers.ts
@@ -1,3 +1,11 @@
+import type { default as OpenTabSettingsPlugin } from "src/main.js"
+
 export async function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+declare module "wdio-obsidian-service" {
+    interface InstalledPlugins {
+        openTabSettings: OpenTabSettingsPlugin,
+    }
 }

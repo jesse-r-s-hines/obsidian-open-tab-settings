d58af4983df4e1fefb6054b74841941c3ed34b94
Jesse Hines <jesse.r.s.hines@gmail.com>
2025-05-25 14:47:53 -0400

    Use reading mode in tests
    This is easier to test on emulate-mobile

--- test/pageobjects/workspace.page.ts
+++ test/pageobjects/workspace.page.ts
@@ -145,11 +145,13 @@ class WorkspacePage {
         await browser.executeObsidianCommand("workspace:toggle-pin");
     }
 
-    async getLink(text: string) {
+    async getLink(text: string): Promise<ChainablePromiseElement> {
         const activeView = $(await browser.executeObsidian(({app, obsidian}) =>
             app.workspace.getActiveViewOfType(obsidian.View)!.containerEl
         ));
-        return activeView.$(`a=${text}`);
+        // In reading view there's some kind of hidden copy of the link that shows up as well, so we have to filter for
+        // the "internal-link" real one.
+        return activeView.$(`.//a[contains(@class, 'internal-link') and text() = '${text}']`)
     }
 
     async openLink(link: ChainablePromiseElement) {
